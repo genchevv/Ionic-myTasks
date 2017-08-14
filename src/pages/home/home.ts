b3f6dda726e1a41b1f22f51aac3990160b444ac1
Radoslav Genchev <r.genchew@gmail.com>
2017-08-14 11:39:27 +0200

    Implemented CRUD functionality for the tasks

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, List } from 'ionic-angular';
+
+import { Task } from './task';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+@ViewChild(List) list: List;
+
+
+	public tasks: Task[];
 
   constructor(public navCtrl: NavController) {
-
+  	this.tasks = [
+  		{id: 1, title: 'Wash the dishes', isDone:false, isEdit: false},
+  		{id: 2, title: 'Call Maria', isDone:false, isEdit: false},
+  		{id: 3, title: 'Watch Udemy Courses', isDone:false, isEdit: false}
+  	];
   }
 
+  addTask(title) {
+  	if(title) {
+  		this.tasks.push(new Task(title));
+  	}
+  }
+
+  editTask(task) {
+  	task.isEdit=!task.isEdit;
+  	this.list.closeSlidingItems();
+  }
+
+  deleteTask(id) {
+  	this.tasks.splice(id, 1);
+  }
 }
